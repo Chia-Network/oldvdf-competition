b85eedb462b3ef627fa21abcbef537f36f073c11
Bram Cohen <bram@chia.networtk>
2018-10-02 18:35:26 -0700

    Made prime selection smoother
    Signed-off-by: Mariano Sorgente <sorgente711@gmail.com>

--- inkfish/create_discriminant.py
+++ inkfish/create_discriminant.py
@@ -3,8 +3,12 @@ from hashlib import sha256
 from .primes import is_probable_prime, odd_primes_below_n
 
 
-odd_primes = odd_primes_below_n(1 << 16)
-sieve_info = [(p, pow(8, p-2, p)) for p in odd_primes]
+# incrementing by an amount with lots of prime factors to make results smoother
+# https://eprint.iacr.org/2011/481.pdf
+odd_primes_above_13 = odd_primes_below_n(1 << 16)[5:]
+m = 8 * 3 * 5 * 7 * 11 * 13
+residues = [x for x in range(7, m, 8) if all([x % y != 0 for y in (3, 5, 7, 11, 13)])]
+sieve_info = [(p, pow(m%p, p-2, p)) for p in odd_primes_above_13]
 
 
 def entropy_from_seed(seed, byte_count):
@@ -24,25 +28,27 @@ def create_discriminant(seed, length=2048):
     Generate a probable prime p where p % 8 == 7.
     Return -p.
     """
-    entropy = entropy_from_seed(seed, length >> 3)
-    n = int.from_bytes(entropy, 'big') | (1 << (length - 1)) | 7
+    entropy = entropy_from_seed(seed, (length >> 3) + 2)
+    n = int.from_bytes(entropy[:-2], 'big') | (1 << (length - 1)) | 7
+    n -= n % m
+    n += residues[int.from_bytes(entropy[-2:], 'big') % len(residues)]
 
-    # Find the smallest prime >= n
+    # Find the smallest prime >= n of the form n + m*x
     while True:
         # we set up "sieve" so it's "True" for known composite numbers
-        sieve = [False] * (1 << 16)  # Store 8*i as i
+        sieve = [False] * (1 << 16)  # Store m*i as i
         for p, q in sieve_info:
-            # q = 8^-1 (mod p)
-            # i = -n / 8, so that 8*i is -n (mod p).
+            # q = m^-1 (mod p)
+            # i = -n / m, so that m*i is -n (mod p).
             i = ((-n % p) * q) % p
             while i < len(sieve):
                 sieve[i] = True
                 i += p
 
         for i, x in enumerate(sieve):
-            if not x and is_probable_prime(n + 8*i):
-                return -(n + 8*i)
-        n += 1 << 19
+            if not x and is_probable_prime(n + m*i):
+                return -(n + m*i)
+        n += m * (1 << 16)
 
 
 """
